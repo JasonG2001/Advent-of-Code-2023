748973f888fa7ca0e462aacc6ac5a52214c474bd
Jason Guan <Jason.Guan21@thehutgroup.com>
2023-12-01 10:16:35 +0000

    Print out day1 part 2 solution

--- problems/day1/Day1.java
+++ problems/day1/Day1.java
@@ -1,7 +1,9 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Day1 {
@@ -9,7 +11,7 @@ public class Day1 {
     public static void main(String[] args) {
         List<String> lines = readFile("day1_input.txt");
         int summ = sumAllEndNumbers(lines);
-        System.out.println(summ);
+        System.out.println(summ); // Part1: 55108, Part 2: 56324
     }
 
     public static List<String> readFile(String fileName) {
@@ -37,22 +39,64 @@ public class Day1 {
         return res;
     }
 
+    public static Map<String, Character> convertNumbers() {
+        Map<String, Character> converter = new HashMap<>() {{
+            put("one", '1');
+            put("two", '2');
+            put("three", '3');
+            put("four", '4');
+            put("five", '5');
+            put("six", '6');
+            put("seven", '7');
+            put("eight", '8');
+            put("nine", '9');
+        }};
+        return converter;
+    }
+
+    public static char containsNumber(String s) {
+        Map<String, Character> converter = convertNumbers();
+        for (Map.Entry<String, Character> entry : converter.entrySet()) {
+            if (s.contains(entry.getKey())) {
+                return entry.getValue();
+            }
+        }
+        return ' ';
+    }
+
     public static int sumEndNumbers(String line) {
         int res = 0;
         int p1 = 0, p2 = line.length() - 1;
-        char leftChar = line.charAt(p1), rightChar = line.charAt(p2);
-        while (!Character.isDigit(leftChar) || !Character.isDigit(rightChar)) {
+        boolean leftFound = false, rightFound = false;
+        char leftChar, rightChar;
+        char leftNum = ' ', rightNum = ' ';
+        while (!leftFound || !rightFound) {
             leftChar = line.charAt(p1);
             rightChar = line.charAt(p2);
-            if (!Character.isDigit(leftChar)) {
-                p1++;
+            if (!leftFound) {
+                if (Character.isDigit(leftChar)) {
+                    leftFound = true;
+                    leftNum = leftChar;
+                } else if (containsNumber(line.substring(0, p1 + 1)) != ' ') {
+                    leftFound = true;
+                    leftNum = containsNumber(line.substring(0, p1 + 1));
+                } else {
+                    p1++;
+                }
             }
-            if (!Character.isDigit(rightChar)) {
-                p2--;
+            if (!rightFound) {
+                if (Character.isDigit(rightChar)) {
+                    rightFound = true;
+                    rightNum = rightChar;
+                } else if (containsNumber(line.substring(p2)) != ' ') {
+                    rightFound = true;
+                    rightNum = containsNumber(line.substring(p2));
+                } else {
+                    p2--;
+                }
             }
         }
-        res += Integer.parseInt(String.valueOf(line.charAt(p1)) + String.valueOf(line.charAt(p2)));
+        res += Integer.parseInt(String.valueOf(leftNum) + String.valueOf(rightNum));
         return res;
-
     }
 }
