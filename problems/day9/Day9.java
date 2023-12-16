81713dd302dce623d37a0f617cc866dea0fd82d9
Jason Guan <Jason.Guan21@thehutgroup.com>
2023-12-16 17:46:33 +0000

    Print answer to part 2

--- problems/day9/Day9.java
+++ problems/day9/Day9.java
@@ -12,10 +12,12 @@ public class Day9 {
 
     private static String fileName = "day9_input.txt";
     private static List<String> fileLines = readFile(fileName);
-    private static long summ = sumAllNums(fileLines);
+    private static long sumNext = sumAllNums(fileLines, true);
+    private static long sumBefore = sumAllNums(fileLines, false);
 
     public static void main(String[] args) {
-        System.out.println(summ); // Part 1: 1782868781
+        System.out.println(sumNext); // Part 1: 1782868781
+        System.out.println(sumBefore); // Part 2: 1057
     }
 
     public static List<String> readFile(String fileName) {
@@ -32,11 +34,11 @@ public class Day9 {
         return fileLines;
     }
 
-    public static long sumAllNums(List<String> fileLines) {
+    public static long sumAllNums(List<String> fileLines, boolean next) {
         long summ = 0;
         for (String line : fileLines) {
             long[] convertedArray = convertToArrayOfLongs(line);
-            summ += getNextNum(convertedArray);
+            summ += getNum(convertedArray, next);
         }
         return summ;
     }
@@ -50,7 +52,7 @@ public class Day9 {
         return convertedArray;
     }
 
-    public static long getNextNum(long[] history) {
+    public static long getNum(long[] history, boolean next) {
         int length = history.length;
         Set<Long> historySet = new HashSet<>();
         for (long value : history) {
@@ -65,6 +67,6 @@ public class Day9 {
         for (int i = 0; i < nextSequence.length; i++) {
             nextSequence[i] = history[i+1] - history[i];
         }
-        return history[length - 1] + getNextNum(nextSequence);
+        return next ? history[length - 1] + getNum(nextSequence, next) : history[0] - getNum(nextSequence, next);
     }
 }
